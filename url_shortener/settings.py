376c8f3487cc7d53aa75e7c841d9c9a2bde9ccb2
Alexsander Lopes Camargos <alexcamargos@gmail.com>
2022-07-02 17:07:42 -0300

    refactor: update database config

--- url_shortener/settings.py
+++ url_shortener/settings.py
@@ -75,10 +75,7 @@ WSGI_APPLICATION = 'url_shortener.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config()
 }
 
 # Password validation
