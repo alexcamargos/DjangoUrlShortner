bea0d71ca397799cbbd9c9379f9f43195e842cbc
Alexsander Lopes Camargos <alexcamargos@gmail.com>
2022-07-02 16:32:14 -0300

    change settings.py to django_on_heroku

--- url_shortener/url_shortener/settings.py
+++ url_shortener/url_shortener/settings.py
@@ -119,4 +119,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-django_heroku.settings(locals())
+django_on_heroku.settings(locals())
