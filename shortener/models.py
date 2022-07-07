ae893c49d58e307dc046a65441307165345a49de
Alexsander Lopes Camargos <alexcamargos@gmail.com>
2022-07-07 19:14:04 -0300

    add redirect count

--- shortener/models.py
+++ shortener/models.py
@@ -1,5 +1,8 @@
+from ipaddress import ip_address
 from django.db import models
+
 from shortuuid.django_fields import ShortUUIDField
+
 
 # Create your models here.
 
@@ -7,6 +10,7 @@ class UniformResourceLocator(models.Model):
     id = models.AutoField(primary_key=True)
     url = models.URLField(max_length=10_000)
     alias = ShortUUIDField(length=6, max_length=6, alphabet="abcdefABCDEF123456", unique=True)
+    created_by = models.CharField(max_length=128, default="")
     created_at = models.DateTimeField(auto_now_add=True)
     edited_at = models.DateTimeField(auto_now=True)
 
@@ -30,3 +34,19 @@ class UniformResourceLocator(models.Model):
     
     def get_url(self):
         return self.url
+    
+    def get_total_clicks(self):
+        clicks = RedirectClickCount.objects.filter(link=self)
+        total_clicks = clicks.aggregate(total_clicks=models.Sum('clicks_count'))['total_clicks']
+
+        return 0 if total_clicks is None else total_clicks
+
+
+class RedirectClickCount(models.Model):
+    link = models.ForeignKey(to=UniformResourceLocator, on_delete=models.CASCADE)
+    clicked_at = models.DateTimeField(auto_now_add=True)
+    ip_address = models.GenericIPAddressField()
+    clicks_count = models.PositiveBigIntegerField(default=0)
+
+    def __str__(self):
+        return f'{self.link.alias} -> {self.clicks_count}'
--- shortener/views.py
+++ shortener/views.py
@@ -1,8 +1,34 @@
 from django.http import HttpResponse
 from django.shortcuts import redirect
 from django.shortcuts import render
+from django.utils import timezone
 
-from .models import UniformResourceLocator
+from .models import UniformResourceLocator, RedirectClickCount
+
+
+def get_user_ip_address(request):
+    """Returns request's ip address."""
+
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+
+    if x_forwarded_for:
+        ip_address = x_forwarded_for.split(',')[0]
+    else:
+        ip_address = request.META.get('REMOTE_ADDR')
+    
+    return ip_address
+
+
+def url_click_processor(request, url):
+    """ Gets or creates a click object and increases its `clicks_count`."""
+
+    # Get the click object for the given url.
+    click = RedirectClickCount.objects.get_or_create(link=url,
+                                                     clicked_at=timezone.now(),
+                                                     ip_address=get_user_ip_address(request))[0]
+    # Increase the clicks count.
+    click.clicks_count += 1
+    click.save()
 
 
 # Create your views here.
@@ -13,7 +39,9 @@ def index(request):
 def create(request):
     if request.method == 'POST':
         link = request.POST['link']
-        new_link = UniformResourceLocator(url=link)
+        user = request.user
+
+        new_link = UniformResourceLocator(url=link, created_by=user)
         new_link.save()
 
         return HttpResponse(new_link.alias)
@@ -21,4 +49,7 @@ def create(request):
 
 def access_url(request, id):
     url = UniformResourceLocator.objects.get(alias=id)
+
+    url_click_processor(request, url)
+
     return redirect(url.get_url())
